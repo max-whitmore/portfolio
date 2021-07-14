24793f0f171da1bcc05ca9175a31884bf376dd72
mxwhtmr <maxcwhitmore@gmail.com>
2021-07-14 21:05:39 +0100

    updates

--- src/context/context.js
+++ src/context/context.js
@@ -5,4 +5,4 @@ const PortfolioContext = React.createContext();
 export const PortfolioProvider = PortfolioContext.Provider;
 export const PortfolioConsumer = PortfolioContext.Consumer;
 
-export default PortfolioContext;
+export default PortfolioContext; 
